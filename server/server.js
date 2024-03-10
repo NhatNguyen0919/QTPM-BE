6011944b30c8db56849d9d9411c81bd89df2d382
NhatNguyen0919 <nhatnha20@uef.edu.vn>
2024-03-10 22:23:48 +0700

    First-commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 8888
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/', (req, res) => {
+    res.send("server on !")
+})
+
+app.listen(port, () => {
+    console.log("server is running", port)
+})
